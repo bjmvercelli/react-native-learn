a2d40b14e08f494da5b3d6111662aecf74b32cf8
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-07-23 11:22:16 -0300

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text, TextInput
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+    <Text>Hello World!</Text>
+    </>
+  );
+};
+
+
+export default App;
